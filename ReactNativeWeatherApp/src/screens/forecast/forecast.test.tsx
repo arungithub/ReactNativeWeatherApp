fecf2e4807c9f568d510924137c6e9b71ce65d4a
Arun HS <arunhs55@gmail.com>
2023-10-17 14:25:59 +0530

    Selected Location Weather information unit test for forecast screen has been added

--- ReactNativeWeatherApp/src/screens/forecast/forecast.test.tsx
+++ ReactNativeWeatherApp/src/screens/forecast/forecast.test.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import Forecast from './forecast';
-import renderer from 'react-test-renderer';
+import { renderWithRedux } from '../../utils/testUtils/testUtils';
+import { fireEvent, waitFor } from '@testing-library/react-native';
+import store from '../../core/redux/store';
+import {
+  getSelectedLocationWeatherForecast,
+  getSelectedLocationWeatherInfo,
+  updateSelectedCity,
+} from '../../core/redux/actions/appActions';
+import {
+  mockCityWeatherData,
+  mockLocationWeatherData,
+  mockWeatherResponse,
+} from '../../utils/testUtils/testMockData';
+import { AppTestIds } from '../../utils/testUtils/testIds';
+import { enableFetchMocks } from 'jest-fetch-mock';
+import { AppConstants } from '../../constants/constants';
+
+enableFetchMocks();
 
 describe('Forecast', () => {
-  it('should render Forecast component', () => {
-    const forecast = renderer.create(<Forecast />).toJSON();
-    expect(forecast).toMatchSnapshot();
+  beforeEach(() => {
+    fetchMock.resetMocks();
+    fetchMock.mockResponse('[]');
+  });
+
+  let arg = {
+    lat: mockLocationWeatherData.coords.latitude,
+    lon: mockLocationWeatherData.coords.longitude,
+    cnt: 3,
+    units: AppConstants.Celsius.value,
+  };
+
+  //ForecastView - selectedLocationWeather
+  it('Selected Location Weather information', async () => {
+    fetchMock.mockResponse(JSON.stringify(mockWeatherResponse));
+    const { getByTestId } = renderWithRedux(<Forecast />);
+    waitFor(async () => {
+      await store.dispatch(updateSelectedCity(mockCityWeatherData));
+      await store.dispatch(getSelectedLocationWeatherInfo(arg));
+    });
+    expect(getByTestId(AppTestIds.ForecastView)).toBeOnTheScreen();
   });
 });
